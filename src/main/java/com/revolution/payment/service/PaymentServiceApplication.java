3cff312802661dfffde822066b00e29e26b0956d
karolwolny <kwolny31@gmail.com>
2025-03-27 18:49:11 +0100

    Update package

--- src/main/java/com/revolution/payment/service/PaymentServiceApplication.java
+++ src/main/java/com/revolution/payment/service/PaymentServiceApplication.java
@@ -1,4 +1,4 @@
-package com.revolution.PaymentService;
+package com.revolution.payment.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
