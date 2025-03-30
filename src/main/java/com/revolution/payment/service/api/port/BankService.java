f42f7d15b5b8eb57ef73de5d0ca0225b1f3be133
karolwolny <kwolny31@gmail.com>
2025-03-30 20:17:39 +0200

    Remove unused import

--- src/main/java/com/revolution/payment/service/api/port/BankService.java
+++ src/main/java/com/revolution/payment/service/api/port/BankService.java
@@ -1,6 +1,5 @@
 package com.revolution.payment.service.api.port;
 
-import com.revolution.payment.service.api.dto.PaymentDto;
 import com.revolution.payment.service.api.dto.PayoutDto;
 import com.revolution.payment.service.api.request.PayoutRequest;
 
